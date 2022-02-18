4ec2a2be928aa2d07f084617b28a6c6485635eb4
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-18 15:00:51 +0100

    finalization for sql requests

--- database/initialization.sql
+++ database/initialization.sql
@@ -11,8 +11,38 @@ CREATE TABLE `users` (
     website VARCHAR(50) NOT NULL
 ) ENGINE = InnoDB;
 
-// posts
-// albums
-// comments
-// todos
-// photos
+CREATE TABLE `posts` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL REFERENCES users(id),
+    title VARCHAR(25) NOT NULL,
+    body TEXT NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE `albums` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL REFERENCES users(id),
+    title VARCHAR(25) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE `comments` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    postId INT NOT NULL REFERENCES posts(id),
+    name VARCHAR(25) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    body TEXT NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE `todos` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL REFERENCES users(id),
+    title VARCHAR(25) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE `photos` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    albumId INT NOT NULL REFERENCES albums(id),
+    title VARCHAR(25) NOT NULL,
+    url VARCHAR(50) NOT NULL,
+    thumbnailUrl VARCHAR(50) NOT NULL
+) ENGINE = InnoDB;
