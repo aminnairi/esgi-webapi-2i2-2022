44fe87a74476174fd7b66acc05d3c97f8ca3b953
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-22 10:51:54 +0100

    prepared statement

--- models/UserModel.php
+++ models/UserModel.php
@@ -9,4 +9,24 @@ class UserModel
         $users = $getUsersQuery->fetchAll();
         return $users;
     }
+
+    public static function create(array $userToCreate)
+    {
+        include "./database/connection.php";
+
+        $email = $userToCreate["email"];
+        $name = $userToCreate["name"];
+        $username = $userToCreate["username"];
+        $phone = $userToCreate["phone"];
+        $website = $userToCreate["website"];
+        $password = password_hash($userToCreate["password"], PASSWORD_BCRYPT);
+        $createUserQuery = $databaseConnection->prepare("INSERT INTO users(name, username, email, phone, website, password) VALUES(':name', ':username', ':email', ':phone', ':website', ':password');");
+        $createUserQuery->bindParam(":name", $name);
+        $createUserQuery->bindParam(":username", $username);
+        $createUserQuery->bindParam(":email", $email);
+        $createUserQuery->bindParam(":phone", $phone);
+        $createUserQuery->bindParam(":website", $website);
+        $createUserQuery->bindParam(":password", $password);
+        $createUserQuery->execute();
+    }
 }
